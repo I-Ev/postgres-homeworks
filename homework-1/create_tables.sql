2ec14daa9850a450c943411e0944f0e87cf14978
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-06-08 13:36:43 +0300

    fix for homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int Primary key,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id char(5) Primary key,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int Primary key,
+	customer_id char(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,44 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import os
+import psycopg2
+
+
+def get_filepath(relative_path):
+    file_path = os.path.join(base_path, relative_path)
+    return file_path
+
+
+base_path = r'C:\PythonProjects\postgres-homeworks'
+
+conn = psycopg2.connect(host="localhost", database='north', user='postgres', password='0000')
+try:
+    with conn:
+        with conn.cursor() as cur:
+
+            relative_path = r'homework-1\north_data\customers_data.csv'
+            file_path = get_filepath(relative_path)
+            with open(file_path, newline='') as f:
+                reader = csv.reader(f)
+                next(reader)  # Пропустить заголовок CSV
+                for row in reader:
+                    cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', row)
+
+            relative_path = r'homework-1\north_data\employees_data.csv'
+            file_path = get_filepath(relative_path)
+            with open(file_path, newline='') as f:
+                reader = csv.reader(f)
+                next(reader)  # Пропустить заголовок CSV
+                for row in reader:
+                    cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', row)
+
+            relative_path = r'homework-1\north_data\orders_data.csv'
+            file_path = get_filepath(relative_path)
+            with open(file_path, newline='') as f:
+                reader = csv.reader(f)
+                next(reader)  # Пропустить заголовок CSV
+                for row in reader:
+                    cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', row)
+
+finally:
+    conn.close()
